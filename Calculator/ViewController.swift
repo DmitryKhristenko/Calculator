5ecfaece45693bada186631a1fb6b27b3130bd5b
Dmitry Khristenko <dmitrykhrist@gmail.com>
2022-11-25 17:23:21 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Дмитрий Х on 25.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
